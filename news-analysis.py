a6d07faed2f70af3137eedfe0e2fbde381d41c01
sreaves <shannonreaves.slp@gmail.com>
2022-06-27 00:32:03 -0500

    done

--- news-analysis.py
+++ news-analysis.py
@@ -39,7 +39,7 @@ from binance.enums import *
 from binance.exceptions import BinanceAPIException, BinanceOrderException
 
 # used for binance websocket
-from websocket import BinanceSocketManager
+import websocket as BinanceSocketManager
 #from binance.streams import BinanceSocketManager
 from twisted.internet import reactor
 
